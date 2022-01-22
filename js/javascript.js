74944ecfe66405b370f4b7341a56af671b0a299a
sho0terbr <rafaelaugusto_leandro@hotmail.com>
2022-01-21 21:36:07 -0300

    estilização marvelix

--- js/javascript.js
+++ js/javascript.js
@@ -8,3 +8,23 @@ window.addEventListener('scroll', function() {
       el.classList.remove('mudaCor'); // remove classe "mudaCor"
     }
 });
+
+
+
+
+// Get the video
+var video = document.getElementById("myVideo");
+
+// Get the button
+var btn = document.getElementById("myBtn");
+
+// Pause and play the video, and change the button text
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
